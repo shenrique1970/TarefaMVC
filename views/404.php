64c416105473f27e2064541e054883968f694e75
Sergio <shvribeiro@hotmail.com>
2021-12-04 15:36:13 -0300

    inicial

---
+++ views/404.php
@@ -0,0 +1,19 @@
+<html> 
+    <head> 
+    <meta charset="utf-8" /> 
+         <title>Gerenciador de Tarefas</title> 
+         <link rel="stylesheet" href="assets/tarefas.css" type="text/css" />
+        <!-- Required meta tags -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head> 
+    <body> 
+        <div id="bloco_principal" class="container-fluid"> 
+            <h1>Ops, página não encontrada</h1> 
+             <p> 
+                 <a href="index.php?rota=tarefas">Ir para a lista de tarefas</a> 
+             </p> 
+         </div> 
+     </body> 
+</html> 
